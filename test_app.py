732d173cb73b77c6511bbf14a2e8c1da6afc0407
joaquin <quinoalias@gmail.com>
2026-03-10 23:31:20 +0100

    Cambio en el mensaje de app.py y test_app.py

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Servidor desplegado correctamente" in response.data
+    assert b"Hola Joaquín, el servidor está funcionando correctamente en Kubernetes!" in response.data
