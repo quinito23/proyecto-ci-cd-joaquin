6ee1f2acb33e460b9c34351758249403250bfba2
joaquin <quinoalias@gmail.com>
2026-03-10 23:37:21 +0100

    Cambio en el mensaje de app.py y test_app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hola Joaquín, el servidor está funcionando correctamente en Kubernetes!"
+    return "Hola Joaquin, el servidor esta funcionando correctamente en Kubernetes!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hola Joaquín, el servidor está funcionando correctamente en Kubernetes!" in response.data
+    assert b"Hola Joaquin, el servidor esta funcionando correctamente en Kubernetes!" in response.data
